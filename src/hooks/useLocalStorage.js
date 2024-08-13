671d6e0d6c485b420a8ffdd4eea5d64dfe674aa1
Fauzan Radji <72055502+fauzan-radji@users.noreply.github.com>
2024-08-13 12:49:01 +0800

    fix: save `useLocalStorage` initial value if there isn't any in the localStorage

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,10 +1,14 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function useLocalStorage(key, initialValue) {
   const [storedValue, setStoredValue] = useState(() => {
     try {
       const item = localStorage.getItem(key);
-      return item ? JSON.parse(item) : initialValue;
+      if (!item) {
+        localStorage.setItem(key, JSON.stringify(initialValue));
+        return initialValue;
+      }
+      return JSON.parse(item);
     } catch (error) {
       console.error(error);
       return initialValue;
@@ -21,8 +25,6 @@ export default function useLocalStorage(key, initialValue) {
       console.error(error);
     }
   };
-
-  useEffect(() => setValue(initialValue), []);
 
   return [storedValue, setValue];
 }
